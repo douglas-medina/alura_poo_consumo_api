77bf620bed06a562a9ea53dbb67c5c66ec5a27f9
Douglas Medina <medina.douglas.dmv@gmail.com>
2024-06-19 11:22:04 -0300

    heranca e desafio 1

---
+++ desafio_modulo1_banco/agencia.py
@@ -0,0 +1,6 @@
+from desafio_modulo1_banco.banco import Banco
+
+class Agencia(Banco):
+    def __init__(self, nome, endereco, numero):
+        super().__init__(nome, endereco)
+        self.numero = numero
