d71aa9adb3c22f4c12ef9d2fa7bfc5ef20bdc455
Douglas Medina <medina.douglas.dmv@gmail.com>
2024-07-01 14:42:20 -0300

    metodo abstrato e polimorfismo

--- app.py
+++ app.py
@@ -3,8 +3,13 @@ from modelos.cardapio.bebida import Bebida
 from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
+
 bebida_suco = Bebida('Suco de Melancia', 5.0, 'Grande')
-prato_paozinho = Prato('Paozinho', 3.0, 'O melhor pão da cidade')
+bebida_suco.aplicar_desconto()
+
+prato_paozinho = Prato('Paozinho', 2.0, 'O melhor pão da cidade')
+prato_paozinho.aplicar_desconto()
+
 restaurante_praca.adicionar_no_cardapio(bebida_suco)
 restaurante_praca.adicionar_no_cardapio(prato_paozinho)
 
